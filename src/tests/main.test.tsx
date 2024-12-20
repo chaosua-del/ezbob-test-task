c6e9edb97af6a49db30bbeeffc25aee3e4a36982
chaosua-del <d.yuhymenko@gmail.com>
2024-12-20 16:25:47 +0100

    feat: fix build errors

--- src/tests/main.test.tsx
+++
@@ -1,9 +0,0 @@
-import { describe, test } from 'vitest';
-import { render } from '@testing-library/react';
-import Main from '../pages/Main';
-
-describe('Testing Main page', () => {
-  test('Should render main', () => {
-    render(<Main />);
-  });
-});
